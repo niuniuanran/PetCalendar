85bde04a2f2281ac0cde336d3084d59ee770b877
niuniuanran <irene.anran.niu@gmail.com>
2020-07-10 22:29:49 +1200

    Fetch image from unsplash

--- pet-calendar/src/Components/ImageList/ImageList.tsx
+++ pet-calendar/src/Components/ImageList/ImageList.tsx
@@ -5,8 +5,29 @@ interface IImagePreference {
   query: string | null;
 }
 
+const unsplash = new Unsplash({
+  accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY || "",
+});
+
 export default function ImageList(props: IImagePreference) {
   const [imageResults, setImageResults] = useState([]);
-  useEffect(function () {}, []);
-  return <div>props.query</div>;
+  useEffect(function () {
+    unsplash.search
+      .photos("dogs", 1, 10, { orientation: "landscape" })
+      .then(toJson)
+      .then((json) => {
+        console.log(json);
+        setImageResults(json.results);
+        console.log(imageResults);
+      });
+  }, []);
+  return (
+    <div>
+      {imageResults.map((result, index) => (
+        <div key={index}>
+          <img src={result["urls"]["small"]} />
+        </div>
+      ))}
+    </div>
+  );
 }
