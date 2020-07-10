946edb017696824b886cc8d7e1dcda4d41981243
niuniuanran <irene.anran.niu@gmail.com>
2020-07-11 00:12:57 +1200

    Create Calendar component

--- pet-calendar/src/App.tsx
+++ pet-calendar/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import ImageCard from "./Components/ImageList/ImageList";
+import Calendar from "./Components/Calendar/Calendar";
 
 function App() {
   return (
     <div className="App">
+      <Calendar />
       <ImageCard query="dog" />
     </div>
   );
