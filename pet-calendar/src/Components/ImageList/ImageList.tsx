d860faad3632ff6eb1e2a0a41e6ec5fb59896bbb
niuniuanran <irene.anran.niu@gmail.com>
2020-07-11 12:52:41 +1200

    Add ImageCredit to unsplash images used

--- pet-calendar/src/Components/ImageList/ImageList.tsx
+++ pet-calendar/src/Components/ImageList/ImageList.tsx
@@ -3,13 +3,22 @@ import Unsplash, { toJson } from "unsplash-js";
 import { makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
-// import GridListTileBar from "@material-ui/core/GridListTileBar";
-// import IconButton from "@material-ui/core/IconButton";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Typography from "@material-ui/core/Typography";
 
 interface IImagePreference {
   query: string | null;
 }
-
+interface IImageInfo {
+  urls: {
+    regular: string;
+  };
+  color: string;
+  user: {
+    name: string;
+    username: string;
+  };
+}
 const unsplash = new Unsplash({
   accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY || "",
 });
@@ -44,13 +53,15 @@ const useStyles = makeStyles((theme) => ({
   },
   titleBar: {
     background:
-      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 60%, rgba(0,0,0,0) 100%)",
   },
 }));
 
 export default function ImageList(props: IImagePreference) {
   const classes = useStyles();
+
   const [imageResults, setImageResults] = useState([]);
+
   useEffect(function () {
     unsplash.search
       .photos("dogs", 1, 10, { orientation: "landscape" })
@@ -60,10 +71,11 @@ export default function ImageList(props: IImagePreference) {
         setImageResults(() => json.results);
       });
   }, []);
+
   return (
     <div>
-      <GridList className={classes.gridList} cols={2.5}>
-        {imageResults.map((result, index) => (
+      <GridList className={classes.gridList} cols={4.1} cellHeight={160}>
+        {imageResults.map((result: IImageInfo, index) => (
           <GridListTile key={index}>
             <img
               src={result["urls"]["regular"]}
@@ -71,10 +83,44 @@ export default function ImageList(props: IImagePreference) {
               className={classes.thumbnailImage}
             />
 
-            {/* <GridListTileBar title={result["description"]} /> */}
+            <GridListTileBar
+              title={
+                <ImageCredit
+                  username={result.user.username}
+                  name={result.user.name}
+                />
+              }
+              className={classes.titleBar}
+            />
           </GridListTile>
         ))}
       </GridList>
     </div>
+  );
+}
+
+interface ICreditInfo {
+  username: string;
+  name: string;
+}
+
+function ImageCredit(props: ICreditInfo) {
+  return (
+    <Typography variant="caption" gutterBottom>
+      Photo by{" "}
+      <a
+        href={`https://unsplash.com/@${props.username}?utm_source=${process.env.REACT_APP_NAME}&utm_medium=referral`}
+        style={{ color: "#fff" }}
+      >
+        {props.name}
+      </a>{" "}
+      on{" "}
+      <a
+        href={`https://unsplash.com/?utm_source=${process.env.REACT_APP_NAME}&utm_medium=referral`}
+        style={{ color: "#fff" }}
+      >
+        Unsplash
+      </a>
+    </Typography>
   );
 }
