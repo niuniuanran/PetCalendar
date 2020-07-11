66c244ef1345a137f11391e40a8290a988c42e5b
niuniuanran <irene.anran.niu@gmail.com>
2020-07-12 00:01:16 +1200

    Add pet thumbnails

--- pet-calendar/src/Commons/petQueries.tsx
+++ pet-calendar/src/Commons/petQueries.tsx
@@ -1,3 +1,32 @@
 export const petQueries = {
-  dog: "dogs dog",
+  dog: {
+    query: "dogs dog",
+    thumbnail: "dog.jpg",
+    credit: { name: "Jamie Street", username: "jamie452" },
+  },
+  deer: {
+    query: "deers deer",
+    thumbnail: "deer.jpg",
+    credit: { name: "Frederick Tubiermont", username: "callmefred" },
+  },
+  cat: {
+    query: "cats cat kitten kittens",
+    thumbnail: "cat.jpg",
+    credit: { name: "Manja Vitolic", username: "madhatterzone" },
+  },
+  bunny: {
+    query: "bunny bunnies",
+    thumbnail: "bunny.jpg",
+    credit: { name: "Waranya Mooldee", username: "anyadiary" },
+  },
+  pony: {
+    query: "ponies pony",
+    thumbnail: "pony.jpg",
+    credit: { name: "Soledad Lorieto", username: "sool_lorieto" },
+  },
+  lamb: {
+    query: "lamb lambs",
+    thumbnail: "lamb.png",
+    credit: { name: "Bill Fairs", username: "moonboyz" },
+  },
 };
