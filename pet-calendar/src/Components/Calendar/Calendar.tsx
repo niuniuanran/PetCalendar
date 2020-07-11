1c9369ebbf872ef586869d88e5846fad4c567933
niuniuanran <irene.anran.niu@gmail.com>
2020-07-11 13:55:12 +1200

    Wire things up into Calendar component

--- pet-calendar/src/Components/Calendar/Calendar.tsx
+++ pet-calendar/src/Components/Calendar/Calendar.tsx
@@ -1,25 +1,102 @@
-import React from "react";
-import moment from "moment";
-import Typography from "@material-ui/core/Typography";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CalendarInfo from "./CalendarInfo";
+import ImageList from "./ImageList";
+import Unsplash, { toJson } from "unsplash-js";
+import { IImageInfo } from "../../Commons/interfaces";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",
-    maxWidth: 500,
+    display: "grid",
   },
+  mainCard: {
+    display: "flex",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  content: {
+    flex: "1 0 auto",
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+}));
+const unsplash = new Unsplash({
+  accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY || "",
 });
 
 export default function Calendar() {
+  const [imageResults, setImageResults] = useState([]);
+  const [currentFocus, setCurrentFocus] = useState(0);
+
+  useEffect(function () {
+    unsplash.search
+      .photos("dogs", 1, 10, { orientation: "landscape" })
+      .then(toJson)
+      .then((json) => {
+        console.log(json.results);
+        setImageResults(() => json.results);
+      });
+  }, []);
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Typography variant="h1" gutterBottom>
-        {moment().format("LL")}
-      </Typography>
-      <Typography variant="h2" gutterBottom>
-        {moment().format("dddd")}
-      </Typography>
+      <Card className={classes.mainCard}>
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <CalendarInfo />
+          </CardContent>
+          <div className={classes.controls}>
+            {/* <IconButton aria-label="previous">
+              {theme.direction === "rtl" ? (
+                <SkipNextIcon />
+              ) : (
+                <SkipPreviousIcon />
+              )}
+            </IconButton>
+            <IconButton aria-label="play/pause">
+              <PlayArrowIcon className={classes.playIcon} />
+            </IconButton>
+            <IconButton aria-label="next">
+              {theme.direction === "rtl" ? (
+                <SkipPreviousIcon />
+              ) : (
+                <SkipNextIcon />
+              )}
+            </IconButton> */}
+          </div>
+        </div>
+        <CardMedia
+          className={classes.cover}
+          image={
+            imageResults[currentFocus] &&
+            imageResults[currentFocus]["urls"]["regular"]
+          }
+          title="Live from space album cover"
+        />
+      </Card>
+
+      <ImageList
+        imageList={imageResults}
+        onClickImage={(index: number) => setCurrentFocus(() => index)}
+        focusIndex={currentFocus}
+      />
     </div>
   );
 }
