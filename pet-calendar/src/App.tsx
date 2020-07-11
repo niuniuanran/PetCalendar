ba1757ce8202e8f7594e70a481c79f31e4500f3a
niuniuanran <irene.anran.niu@gmail.com>
2020-07-11 13:56:16 +1200

    Use Calendar component in App.tsx

--- pet-calendar/src/App.tsx
+++ pet-calendar/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import ImageCard from "./Components/ImageList/ImageList";
+// import ImageCard from "./Components/Calendar/ImageList";
+// import CalendarInfo from "./Components/Calendar/CalendarInfo";
 import Calendar from "./Components/Calendar/Calendar";
 
 function App() {
   return (
     <div className="App">
+      {/* <CalendarInfo />
+      <ImageCard query="dog" /> */}
       <Calendar />
-      <ImageCard query="dog" />
     </div>
   );
 }
